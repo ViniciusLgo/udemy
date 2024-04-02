2b3908965272ba4557ff437cbb4f3b6e4e764966
ViniciusLgo <vinicruzlago@gmail.com>
2024-04-02 16:11:30 -0300

    final card read more

---
+++ other-projects/CardReadMore/assets/js/script.js
@@ -0,0 +1,13 @@
+var button = document.getElementById('read_button');
+
+button.addEventListener('click', function() {
+    var card = document.querySelector('.card');
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        button.innerText = 'Read Less';
+    } else {
+        button.innerText = 'Read More';
+    }
+
+});
